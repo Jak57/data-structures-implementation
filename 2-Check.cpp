590c6bf573c4153f3443c57c8b02381ac9339e17
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2021-03-20 13:11:42 +0600

    Update 2-Check.cpp

--- 2-Check.cpp
+++ 2-Check.cpp
@@ -18,3 +18,24 @@ int main()
 /*
 22 2
 */
+
+//Another Approach
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, cnt = 0;
+    cin>> n;
+
+    while(n > 0){
+        cnt++;
+        n = (n & (n-1));
+    }
+
+    cout<< cnt << "\n";
+
+    return 0;
+}
