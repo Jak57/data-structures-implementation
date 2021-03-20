dffc63570bff20816417efacda6ebece50d3ef39
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2021-03-20 12:21:07 +0600

    Add files via upload

---
+++ 1-Shift.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, i;
+    cin>> n >> i;
+
+    cout<< (n << i) << " " << (n >> i) << "\n";
+
+    return 0;
+}
+/*
+100 5
+*/
