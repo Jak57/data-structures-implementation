69dd4a4070736c65d3c35bbfca98c81a61c88c82
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2021-04-05 13:55:45 +0600

    Update 4-Subset Sum.cpp

--- 4-Subset Sum.cpp
+++ 4-Subset Sum.cpp
@@ -1,3 +1,4 @@
+1.
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -54,3 +55,102 @@ int main()
 4 12
 2 5 3 4
 */
+
+2.
+// Another Approach O(m) memory:
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, i, j, sum, coin[10000], dp[100000], val;
+    cin>> n >> sum;
+
+    for(i = 1; i <= n; i++)
+        cin>> coin[i];
+
+    dp[0] = 1;
+    for(i = 1; i <= sum; i++)
+        dp[i] = 0;
+
+    for(i = 1; i <= n; i++)
+    {
+
+        for(j = sum; j >= coin[i]; j--)
+        {
+            if(dp[j] == 0)
+            {
+                val = coin[i];
+                if(dp[j-val] != 0)
+                    dp[j] = 1;
+            }
+
+        }
+    }
+
+    for(i = 0; i <= sum; i++)
+        cout<< dp[i] << " ";
+    cout<< "\n";
+
+
+    return 0;
+}
+/*
+4 12
+2 5 3 4
+*/
+3. 
+// Printing solution
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, i, j, sum, coin[10000], dp[100000], val;
+    cin>> n >> sum;
+
+    for(i = 1; i <= n; i++)
+        cin>> coin[i];
+
+    dp[0] = 1;
+    for(i = 1; i <= sum; i++)
+        dp[i] = 0;
+
+    for(i = 1; i <= n; i++)
+    {
+
+        for(j = sum; j >= coin[i]; j--)
+        {
+            if(dp[j] == 0)
+            {
+                val = coin[i];
+                if(dp[j-val] != 0)
+                    dp[j] = coin[i];
+            }
+
+        }
+    }
+
+    i = sum;
+    while(i > 0)
+    {
+        cout<< dp[i] << " ";
+        i = i - dp[i];
+    }
+
+
+    return 0;
+}
+/*
+4 12
+2 5 3 4
+*/
+
+
+
+
+
