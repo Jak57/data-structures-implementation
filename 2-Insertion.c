9a9cb62779f3a2012cd5dac49ec7d10627b38787
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2021-04-25 09:55:57 +0600

    Add files via upload

---
+++ 2-Insertion.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
+int main(void)
+{
+    node *list = NULL;
+    node *aux = NULL;
+
+    for (int i = 0; i < 4; i++)
+    {
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            // Freeing heap memory
+            while (list != NULL)
+            {
+                node *tmp = list->next;
+                free(list);
+                list = tmp;
+            }
+            return 1;
+        }
+
+        n->number = i + 1;
+        n->next = NULL;
+
+        if (i == 0)
+        {
+            list = n;
+            aux = n;
+        }
+        else
+        {
+            aux->next = n;
+            aux = n;
+        }
+    }
+
+    // Inserting node in beginning
+    // O(1) insertion
+    node *n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+
+    n->number = 5;
+    n->next = NULL;
+
+    n->next = list;
+    list = n;
+
+    for (node *tmp = list; tmp != NULL; tmp = tmp->next)
+    {
+        printf("%d %p\n", tmp->number, tmp->next);
+    }
+
+    while (list != NULL)
+    {
+        node *tmp = list->next;
+        free(list);
+        list = tmp;
+    }
+
+}
+
