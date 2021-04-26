b3d9f4d1bf82c85aeb46828b167c2e2981a7d5d3
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2021-04-26 17:32:55 +0600

    Update 2-Insertion.c

--- 2-Insertion.c
+++ 2-Insertion.c
@@ -1,3 +1,4 @@
+1. // Insert at beginning
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -71,3 +72,76 @@ int main(void)
 
 }
 
+2. // Insert at middle
+    
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
+int main(void)
+{
+    node *list = NULL;
+    node *aux = NULL;
+    
+    for (int i = 0; i < 4; i++)
+    {
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            // Freeing heap memory
+            while (list != NULL)
+            {
+                node *tmp = list->next;
+                free(list);
+                list = tmp;
+            }
+            return 1;
+        }
+        
+        n->number = i + 1;
+        n->next = NULL;
+        
+        if (i == 0)
+        {
+            list = n;
+            aux = n;
+        }
+        else 
+        {
+            aux->next = n;
+            aux = n;
+        }
+    }
+    
+    for (node *tmp = list; tmp != NULL; tmp = tmp->next)
+    {
+        if (tmp->number == 3)
+        {
+            node *n = malloc(sizeof(node));
+            if (n == NULL)
+            {
+                return 1;
+            }
+            
+            n->number = 5;
+            n->next = tmp->next;
+            tmp->next = n;
+            
+        }
+    }
+    
+    while (list != NULL)
+    {
+        printf("%d\n", list->number);
+        node *tmp = list->next;
+        free(list);
+        list = tmp;
+    }
+    
+}
