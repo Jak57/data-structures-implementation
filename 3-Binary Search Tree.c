ebec280cf7febb8cc37a9c6ac0517f4478e76913
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2021-04-27 11:19:14 +0600

    Update 3-Binary Search Tree.c

--- 3-Binary Search Tree.c
+++ 3-Binary Search Tree.c
@@ -183,4 +183,126 @@ int main(void)
     return 0;
 
 }
+//3. Correct code. Printing tree, freeing tree and searching
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct node
+{
+    int number;
+    struct node *left;
+    struct node *right;
+}
+node;
+
+void free_tree(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    free_tree(root->left);
+    free_tree(root->right);
+    free(root);
+}
+
+void print_tree(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    
+    print_tree(root->left);
+    printf("%d\n", root->number);
+    print_tree(root->right);
+}
+
+bool binarySearchTree(node *root, int number)
+{
+    if (root == NULL)
+    {
+        return false;
+    }
+    else if (number < root->number)
+    {
+        return binarySearchTree(root->left, number);
+    }
+    else if (number > root->number)
+    {
+        return binarySearchTree(root->right, number);
+    }
+    else
+    {
+        return true;
+    }
+
+}
+
+int main(void)
+{
+    node *root = NULL;
+    node *n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+
+    n->number = 4;
+    n->left = NULL;
+    n->right = NULL;
+    root = n;
+
+    n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+
+    n->number = 2;
+    n->left = NULL;
+    n->right = NULL;
+    root->left = n;
+
+    n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+
+    n->number = 5;
+    n->left = NULL;
+    n->right = NULL;
+    root->right = n;
+
+    n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+
+    n->number = 1;
+    n->left = NULL;
+    n->right = NULL;
+    root->left->left = n;
+
+    n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+
+    n->number = 3;
+    n->left = NULL;
+    n->right = NULL;
+    root->left->right = n;
+    
+    print_tree(root);
+    free_tree(root);
+
+    return 0;
+
+}
 
