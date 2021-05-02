a90b07144a3b5c36cc5019fb0f28b264ad3765ac
Jakir Hasan <52996649+Jak57@users.noreply.github.com>
2021-05-02 10:27:30 +0600

    Update 5-Stack.c

--- 5-Stack.c
+++ 5-Stack.c
@@ -1,3 +1,4 @@
+//1. Using array
 #include <stdio.h>
 
 const int CAPACITY = 100000;
@@ -64,6 +65,98 @@ int main(void)
     // Size of stack
 
     printf("Size: %d\n", s.top + 1);
+
+
+}
+
+//2. Using Linked list
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int value;
+    struct node *next;
+}
+node;
+
+// Declaring head globally so that don't have to pass address
+node *head = NULL;
+
+int size()
+{
+    int count = 0;
+    for (node *tmp = head; tmp != NULL; tmp = tmp->next)
+    {
+        count++;
+    }
+
+    return count;
+}
+
+int pop()
+{
+    int value;
+    node *tmp = head->next;
+
+    value = head->value;
+    free(head);
+    head = tmp;
+
+    return value;
+}
+
+void push(int value)
+{
+    //printf("%p\n", head);
+    node *n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        return;
+    }
+
+    n->value = value;
+    n->next = NULL;
+
+    if (head == NULL)
+    {
+        head = n;
+    }
+    else
+    {
+        n->next = head;
+        head = n;
+    }
+}
+
+int main(void)
+{
+    int value, x;
+    //stack *head = NULL;
+
+    value = 1;
+    push(value);
+    printf("%d\n", head->value);
+
+    value = 2;
+    push(value);
+    printf("%d\n", head->value);
+
+    value = 3;
+    push(value);
+    printf("%d\n", head->value);
+
+    printf("Size: %d\n", size());
+
+    x = pop();
+    printf("%d\n", x);
+
+    x = pop();
+    printf("%d\n", x);
+
+    x = pop();
+    printf("%d\n", x);
+
 
 
 }
